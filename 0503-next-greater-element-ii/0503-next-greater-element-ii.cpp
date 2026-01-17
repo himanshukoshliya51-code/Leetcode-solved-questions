dfb2001877ebd70626de7542b8623bd95f8224d8
himanshukoshliya51-code <himanshukoshliya51@gmail.com>
2026-01-17 12:53:49 +0530

    Time: 4 ms (58.11%), Space: 28.5 MB (35.06%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, -1);
+        stack<int> st; // stores indices
+
+        for (int i = 2*n - 1; i >= 0; i--) {
+            int idx = i % n;
+
+            while (!st.empty() && nums[st.top()] <= nums[idx]) {
+                st.pop();
+            }
+
+            if (i < n) {
+                ans[idx] = st.empty() ? -1 : nums[st.top()];
+            }
+
+            st.push(idx);
+        }
+        return ans;
+    }
+};
