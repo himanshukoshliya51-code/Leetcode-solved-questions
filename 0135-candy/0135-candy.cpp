83cf2976fd1e8c2ee6243b039fb00d86ae7f9d08
himanshukoshliya51-code <himanshukoshliya51@gmail.com>
2026-01-30 10:49:03 +0530

    Time: 0 ms (100%), Space: 22 MB (91.99%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        int i = 1;
+        int sum = 1;
+
+        while(i<n){
+              if(ratings[i]==ratings[i-1]){
+                sum += 1;
+                i++;
+                
+            }
+
+            int peak = 1;
+            while(i<n && ratings[i]>ratings[i-1]){
+                 peak++;
+                sum += peak;
+                // peak++;
+                i++;
+            }
+
+            int down = 1;
+            while(i<n && ratings[i]<ratings[i-1]){
+                //down++;
+                sum += down;
+                down++;
+                i++;
+            }
+
+            if(down>peak){
+                sum += (down-peak);
+            }
+        }
+        return sum;
+
+        
+    }
+};
