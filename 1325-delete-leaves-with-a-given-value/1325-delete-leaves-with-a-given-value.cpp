36ea018bd583f1a60f8dc7911f4afb48ae0bc04d
himanshukoshliya51-code <himanshukoshliya51@gmail.com>
2026-02-03 14:01:40 +0530

    Time: 0 ms (100%), Space: 23.4 MB (13.73%) - LeetHub

---
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    TreeNode* solve(TreeNode* root, int target) {
+        if (root == NULL) return NULL;
+
+        root->left = solve(root->left, target);
+        root->right = solve(root->right, target);
+
+        if (root->val == target && 
+            root->left == NULL && 
+            root->right == NULL) {
+           // delete root;
+            return NULL;
+        }
+
+        return root;
+    }
+
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        return solve(root, target);
+    }
+};
