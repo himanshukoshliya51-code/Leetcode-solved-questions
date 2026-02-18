2d359425fda313d6bd4726f6f98c3ae4c951bb2d
himanshukoshliya51-code <himanshukoshliya51@gmail.com>
2026-02-18 17:40:53 +0530

    Time: 0 ms (100%), Space: 61 MB (27.65%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -1,55 +1,24 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-        void inorder(TreeNode* root ){
-            if(root == NULL)return;
-            inorder(root->left);
-            ans.push_back(root->val);
-            inorder(root->right);
+    TreeNode *first = NULL, *second = NULL, *prev = NULL;
+
+    void inorder(TreeNode* root) {
+        if (!root) return;
+
+        inorder(root->left);
+
+        if (prev && root->val < prev->val) {
+            if (!first)
+                first = prev;
+            second = root;
         }
-               void compare(TreeNode* root,vector<int>two){
-                if(root == NULL)return;
-                if(root->val == two[0] ){
-                    root->val = two[1];
-                }
-                else if(root->val == two[1]){
-                    root->val = two[0];
-                }
-                compare(root->left,two);
-                compare(root->right,two);
 
-               }
+        prev = root;
+        inorder(root->right);
+    }
 
-vector<int>ans;
     void recoverTree(TreeNode* root) {
         inorder(root);
-        vector<int>two;
-        vector<int>sorted = ans ;
-        for(int i=0;i<ans.size();i++){
-            sorted[i]=ans[i];
-        }
-        sort(sorted.begin(),sorted.end());
-
-        for(int i=0;i<ans.size();i++){
-            if(ans[i] != sorted[i]){
-                two.push_back(ans[i]);
-                two.push_back(sorted[i]);
-                break;
-            }
-        }
-
-        compare(root,two);
-
-        return ;
+        swap(first->val, second->val);
     }
 };
