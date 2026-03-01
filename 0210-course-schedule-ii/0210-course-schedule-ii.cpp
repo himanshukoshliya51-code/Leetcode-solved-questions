1f4669f41abe1e6ccbf8268a2ed72a8bf85adc0c
himanshukoshliya51-code <himanshukoshliya51@gmail.com>
2026-03-01 15:34:59 +0530

    Time: 13 ms (8.31%), Space: 19.1 MB (22.84%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+     
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses, 0);
+
+       
+        for(auto edge : prerequisites) {
+            int u = edge[0];   
+            int v = edge[1];  
+            
+            adj[v].push_back(u);   
+            indegree[u]++;         
+        }
+
+        queue<int> q;
+
+        // Push all nodes with indegree 0
+        for(int i = 0; i < numCourses; i++) {
+            if(indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+       vector<int>topo;
+
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for(int neighbor : adj[node]) {
+                indegree[neighbor]--;
+                if(indegree[neighbor] == 0) {
+                    q.push(neighbor);
+                }
+            }
+        }
+
+       if(topo.size()==numCourses){
+        return topo;
+       }
+       return {};
+        
+    }
+};
