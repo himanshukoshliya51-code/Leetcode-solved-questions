f5f5e7cc653da209e9b9af59391223df9b75f9cf
Himanshu Yadav <himanshukoshliya51@gmail.com>
2026-03-31 15:09:22 +0530

    Time: 15 ms (29.44%), Space: 18.2 MB (38.8%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,47 +1,31 @@
 class Solution {
 public:
-    int maxProfit(int k, vector<int>& prices) {
-        int n = prices.size();
-        
-        if (k == 0 || n == 0) return 0;
+    int f(int i, int buy, int cnt, vector<int>& prices, vector<vector<vector<int>>>& dp) {
+        if (i == prices.size() || cnt == 0) return 0;
 
-        // Optimization
-        // if (k >= n / 2) {
-        //     int profit = 0;
-        //     for (int i = 1; i < n; i++) {
-        //         if (prices[i] > prices[i - 1])
-        //             profit += prices[i] - prices[i - 1];
-        //     }
-        //     return profit;
-        // }
+        if (dp[i][buy][cnt] != -1) return dp[i][buy][cnt];
 
-        vector<vector<int>> after(2, vector<int>(k + 1, 0));
-        vector<vector<int>> curr(2, vector<int>(k + 1, 0));
+        int profit = 0;
 
-        for (int i = n - 1; i >= 0; i--) {
-
-            // ✅ reset to avoid garbage values
-            curr = vector<vector<int>>(2, vector<int>(k + 1, 0));
-
-            for (int buy = 0; buy <= 1; buy++) {
-                for (int cnt = 1; cnt <= k; cnt++) {
-
-                    if (buy == 1) {
-                        int take = -prices[i] + after[0][cnt];
-                        int nottake = after[1][cnt];
-                        curr[buy][cnt] = max(take, nottake);
-                    } 
-                    else {
-                        int sell = prices[i] + after[1][cnt - 1];
-                        int notsell = after[0][cnt];
-                        curr[buy][cnt] = max(sell, notsell);
-                    }
-                }
-            }
-
-            after = curr;
+        if (buy) {
+            int take = -prices[i] + f(i + 1, 0, cnt, prices, dp);
+            int nottake = f(i + 1, 1, cnt, prices, dp);
+            profit = max(take, nottake);
+        } 
+        else {
+            int sell = prices[i] + f(i + 1, 1, cnt - 1, prices, dp);
+            int notsell = f(i + 1, 0, cnt, prices, dp);
+            profit = max(sell, notsell);
         }
 
-        return after[1][k];
+        return dp[i][buy][cnt] = profit;
+    }
+
+    int maxProfit(int k, vector<int>& prices) {
+        int n = prices.size();
+
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k + 1, -1)));
+
+        return f(0, 1, k, prices, dp);
     }
 };
