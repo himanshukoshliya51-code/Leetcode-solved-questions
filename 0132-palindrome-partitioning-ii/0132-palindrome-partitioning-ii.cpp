f0fb00f2a6ebf60528c66e0a82939726e0d4557e
Himanshu Yadav <himanshukoshliya51@gmail.com>
2026-04-03 13:14:13 +0530

    Time: 1507 ms (34.83%), Space: 9.4 MB (77.62%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -9,26 +9,20 @@ bool ispalind(int i, int j, string& s) {
     }
     return true;
 }
-
-int f(int i,int n, string& s, vector<int>& dp){
-    if(i == n)return 0;
-
-    if(dp[i] != -1)return dp[i];
- 
-int mini = INT_MAX;
-    for(int j = i;j<n;j++){
-        if(ispalind(i,j,s)){
-        int cuts = 1+ f(j+1,n,s,dp);
-         mini = min(mini,cuts);
-        }
-
-    }
-    return dp[i] = mini;
-}
     int minCut(string s) {
         int n = s.size();
-       vector<int>dp(n,-1);
+       vector<int>dp(n+1,0);
 
-        return f(0,n,s,dp)-1;
+       for(int i = n-1;i>=0;i--){
+        int mini = INT_MAX;
+    for(int j = i;j<n;j++){
+        if(ispalind(i,j,s)){
+        int cuts = 1+ dp[j+1];
+         mini = min(mini,cuts);
+        }
+    }
+    dp[i] = mini;
+       }
+        return dp[0]-1;
     }
 };
